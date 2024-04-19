320666bb78d9d99f1f78079a7de3d9992e5a2368
envygodd <bilguun.m@tanasoft.mn>
2024-04-19 17:37:48 +0800

    setToken home

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import "./Home.css"
-const Home = () => {
+import React, { useState, useEffect } from 'react';
+import "./Home.css";
+
+interface TokenType {
+  accessToken: string;
+}
+
+const Home: React.FC = () => {
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('accessToken');
+    if (storedToken){
+      setToken(storedToken)
+    }
+  }, [localStorage.getItem('accessToken')]);
+
+  console.log(token);
+
   return (
     <div>
       Home
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/Loginform/loginform.tsx
+++ src/Components/Loginform/loginform.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { FaLock, FaUser } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
